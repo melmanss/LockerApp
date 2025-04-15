284c9d93471d4e8e94774627475c03aadd3428ea
melmanss <cfhfyjdbx@gmail.com>
2025-04-15 17:31:25 +0300

    New commit

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,4 +1,17 @@
 package app;
 
+
 public class DataHandler {
+
+    private final Locker lock = new
+
+    public synchronized int modify(int num) {
+        lock.();
+        try {
+            num = num * 3;
+            return num;
+        } finally {
+            lock.unlock();
+        }
+    }
 }
