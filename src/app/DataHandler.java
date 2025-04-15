7e3c7957dcf74f0e1824057fd86679451c9d7af5
melmanss <cfhfyjdbx@gmail.com>
2025-04-15 17:35:00 +0300

    Update

--- src/app/DataHandler.java
+++ src/app/DataHandler.java
@@ -1,12 +1,15 @@
+
 package app;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class DataHandler {
 
-    private final Locker lock = new
+    private final Lock lock = new ReentrantLock();
 
-    public synchronized int modify(int num) {
-        lock.();
+    public int modify(int num) {
+        lock.lock();
         try {
             num = num * 3;
             return num;
